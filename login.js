923047676e72c2f9f7f5f77f7400e2cf7c66ceff
Aditya-Singh-Chandel <143488087+Aditya-Singh-Chandel@users.noreply.github.com>
2023-12-03 16:51:29 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function submitForm() {
-    alert('Welcome to the login page');
+    alert('Welcome Back!');
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     console.log('Logging in with:', username, password);
